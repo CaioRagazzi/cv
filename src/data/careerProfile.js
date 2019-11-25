a53d2af118f5da89c57f0805aa56e8e42416e8b6
Caio Ragazzi <ca.ragazzi@gmail.com>
2019-11-25 05:20:26 -0300

    career profile modification

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -1,7 +1,7 @@
 export const careerProfile = {
   sectionTitle: 'Career Profile',
   icon: null,
-  description: '<p>I\'m a full stack developer, currently working with Web API builds / maintenance using .Net Core, DDD, TDD, Entity Framework Core among others.</br>I also work as a front-end and back-end developer using VueJs / ReactJs and NodeJs with express (web api) and mobile using React Native.</br>Always looking to expand my knowledge in the area of ​​software development for web and mobile applications.<br/>I particularly love frameworks that use JavaScript.<br/>Graduated in Systems Analysis and Development at Anhembi Morumbi University.</p>'
+  description: '<p>Full Stack developer.</br>I am specialized in JavaScript Frameworks such as Node, React, React Native and Vue.</br>Also, I have professional experience working with C# with .Net Core, TDD, DDD, Entity and more.</br>Always looking to expand my knowledge in the area of ​​software development for web and mobile applications.<br/>I particularly love frameworks that use JavaScript.<br/>Graduated in Systems Analysis and Development at Anhembi Morumbi University.</p>'
 };
 
 export default careerProfile;
